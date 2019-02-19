fdb2d6e75bf80be6a8b20109e3c7bf20f73bdda6
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-19 22:24:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,12 @@ def line (array)
   if array.length == 0
     puts "The line is currently empty."
   end
+else
+  n = 1
+  new_array = []
+  array.each do |name|
+    new_array.push("#{n}. #{name}")
+  end
+  puts "The line is currently: #{new_array}"
+end
 end
